0e5754b629a14483b3e070764be722b5903c303f
Kevin Quinn <kq1465@gmail.com>
2020-09-17 11:43:42 -0400

    updated for organization

---
+++ add_counties.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+import os
+
+def add_the_cases(univ, county):
+    univ_counties = univ.County
+    county = county.loc[county.Country_Region == "US"]
+    active = []
+    confirmed = []
+    for i in univ_counties:
+        try:
+            row = county.loc[county.Admin2 == i]
+            case = row.Active.iloc[0]
+            total = row.Confirmed.iloc[0]
+            active.append(case)
+            confirmed.append(total)
+        except:
+            active.append(None)
+            confirmed.append(None)
+
+    act_cases = pd.Series(active)
+    tot_cases = pd.Series(confirmed)
+    cases = pd.concat([act_cases, tot_cases], axis = 1)
+    cases.columns = ["County_Active_Cases", "County_Total_Cases"]
+    return cases
+
+cities = pd.read_csv(r"C:\Users\kq146\code\Covid_data\US_Counties\csv\us_cities.csv")
+#county_frame = pd.read_csv(r"C:\Users\kq146\code\Covid_data\collected_data\county_data.csv")
+
+start = dt.date(2020, 9,8)
+end = dt.date.today()
+delta = dt.timedelta(days=1)
+while start <= end:
+    date_string = start.strftime("%m_%d_%y")
+    other_date = start.strftime("%m-%d-%Y")
+    filename = r"C:\Users\kq146\code\Covid_data\UniversityCases\university_cases_"
+    filename = filename + date_string + ".csv"
+    university = pd.read_csv(filename)
+    county_file = r"C:\Users\kq146\code\Covid_data\COVID-19\csse_covid_19_data\csse_covid_19_daily_reports"
+    county_file = os.path.join(county_file, other_date)
+    county_frame = pd.read_csv(county_file + ".csv")
+    start += delta
+
+    university = university.loc[:, ["School", "Cases", "County", "State", "Date"]]
+    university.columns = ["School", "Cases", "City", "State", "Date"]
+    u_loc = university.loc[:,["City", "State"]]
+    states = u_loc.State.unique()
+
+    counties = []
+    for i in states:
+        uni_city = u_loc.loc[u_loc.State == i]
+        lookup = cities.loc[cities.STATE_NAME == i]
+
+        for j in uni_city.City:
+            location = lookup.loc[lookup.CITY == j]
+            try:
+                counties.append(location.COUNTY.iloc[0])
+            except:
+                counties.append(None)
+
+
+    county_series = pd.Series(counties)
+    county_series.columns = ["County"]
+    combined = pd.concat([university, county_series], axis = 1, ignore_index = True)
+    combined.columns = ["School", "Cases", "City", "State", "Date", "County"]
+    combined = combined[["School", "Cases", "City", "County", "State", "Date"]]
+
+    count_cases = add_the_cases(combined, county_frame)
+    with_cases = pd.concat([combined, count_cases], axis = 1)
+    #with_cases.columns = ["School", "Cases", "City", "County", "State", "Date", "County_Cases"]
+    with_cases = with_cases[["School", "Cases", "County_Active_Cases","County_Total_Cases", "City", "County", "State", "Date"]]
+    with_cases.to_csv(r"C:\Users\kq146\code\Covid_data\UniversityCases\university_cases_" + date_string + ".csv")
---
+++ nytimes_scraper.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import datetime as dt
+
+
+driver = webdriver.Chrome()
+driver.get("https://www.nytimes.com/interactive/2020/us/covid-college-cases-tracker.html?referringSource=articleShare")
+html = driver.page_source
+
+soup = BeautifulSoup(html, "html.parser")
+#school_list = soup.find("div", id = "schoolList")
+school_list = soup.select('body > div > main > article > section > div > div.g-story.g-freebird.g-max-limit ')
+school_list = school_list[0].select('#searchlist > div > .list_container > #schoolList')
+state = list(school_list[0].children)
+
+table = []
+for i in range(len(state)):
+    #s_name = state[i].find_all("div", class_= "statename")
+    s_name = state[i].select('div > div > .statename')
+    name = [s_name[0].get_text(strip = True)]
+    #state_schools = state[i].find_all("div", class_="schoolcard individualschools")
+    state_schools = state[i].select('div > .schoolholder > div')
+
+
+    for j in state_schools:
+        ind_school = [text for text in j.stripped_strings]
+        if ind_school != []:
+            ind_school += name
+            table.append(ind_school)
+
+frame = pd.DataFrame(table)
+frame = frame.drop(frame.iloc[:,[3,4]], axis = 1)
+frame.columns = ["School", "Cases", "City", "State"]
+today = dt.date.today()
+this = today.strftime("%m_%d_%y")
+today = today.strftime("%m-%d-%Y")
+
+frame["Date"] = today
+print(frame)
+
+
+file_name = r"\university_cases_" + this + ".csv"
+frame.to_csv(r"C:\Users\kq146\code\Covid_data\UniversityCases" + file_name)
+
+
+driver.quit()
