1f2bcf96b98edfd3a5a1a5820af0e01c2c1b6ee5
Kevin Quinn <quinnk@scc1.bu.edu>
2021-12-20 08:37:14 -0500

    further tests

--- data_tester.py
+++ data_tester.py
@@ -8,7 +8,9 @@ X = pd.read_csv("./analysis/testing_data/D_4.csv", index_col = 0).to_numpy()
 
 state_L = pd.read_csv("./collected_data/state_laplacian.csv", index_col = 0).to_numpy()
 
-G = gridSearcher(X, state_L, saver = "./analysis/testing_data/test_search.csv")
-ranks = list(range(1,3))
-betas = list(range(1,2))
+G = gridSearcher(X, state_L, saver = "./analysis/testing_data/test_search2.csv")
+ranks = list(range(1,50))
+betas = np.linspace(0.0001,2,31)
 G.grid_search(ranks, betas)
+
+print("made it!")
