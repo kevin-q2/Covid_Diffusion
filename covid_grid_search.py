d60385f1d4b65bd4a9587ab56eda88ffc4817746
Kevin Quinn <kq1465@gmail.com>
2022-01-03 17:53:21 -0500

    covid grid search

--- covid_grid_search.py
+++ covid_grid_search.py
@@ -1,55 +1,46 @@
-import sys
-import os
 import numpy as np
 import pandas as pd
-from parameter_testing import *
+import grid_search
+import matrix_operation
+from grid_search import gridSearcher
+from matrix_operation import mat_opr
+import time
 
-cwd = os.getcwd()
-par = os.path.dirname(cwd)
-sys.path.append(par)
+###########################################
+#
+# For performing a grid parameter search over
+# our real covid-19 data
+#
+###########################################
 
-if __name__ == "__main__":
-    # import and clean data
+# case counts 
+#state_dset = pd.read_csv(os.path.join(par, 'collected_data/state_dataset.csv'), index_col = 0)
+state_dset = pd.read_csv('./collected_data/state_dataset.csv', index_col = 0)
+state_dset = mat_opr(state_dset)
+
+# population data
+population = pd.read_csv('./collected_data/state_census_estimate.csv', index_col = 'NAME')
+
+# clean + normalize
+state_iso = state_dset.known_iso()
+pop_dict = {}
+for col in state_iso.dataframe.columns:
+    pop_dict[col] = population.loc[col,'POP']
+
+state_norm = state_iso.population_normalizer(pop_dict)
 
 
-    # case counts 
-    #state_dset = pd.read_csv(os.path.join(par, 'collected_data/state_dataset.csv'), index_col = 0)
-    state_dset = pd.read_csv('./collected_data/state_dataset.csv', index_col = 0)
-    state_dset = mat_opr(state_dset)
+# adjacency Laplacian
+state_L = pd.read_csv("./collected_data/state_laplacian.csv", index_col = 0).to_numpy()
 
-    # population data
-    population = pd.read_csv('./collected_data/state_census_estimate.csv', index_col = 'NAME')
+# grid search over selected list of parameters to find the best
+ranks = list(range(1,20))
+betas = np.linspace(0.01,5,50)
 
-    # clean + normalize
-    state_iso = state_dset.known_iso()
-    pop_dict = {}
-    for col in state_iso.dataframe.columns:
-        pop_dict[col] = population.loc[col,'POP']
+start = time.time()
+G = gridSearcher(state_norm.dataframe, laplacian = state_L, algorithm = "diffusion", max_iter = 20000, tolerance = 1e-8, saver = "./analysis/testing_data/covid_state_grid_search.csv")
+G.grid_search(ranks, betas)
+end = time.time()
 
-    state_norm = state_iso.population_normalizer(pop_dict)
-
-
-    # adjacency Laplacian
-    state_L = pd.read_csv("./collected_data/state_laplacian.csv", index_col = 0).to_numpy()
-
-    '''
-    W = pd.read_csv('./analysis/testing_data/W_test.csv',index_col=0)
-    H = pd.read_csv('./analysis/testing_data/H_test.csv',index_col=0)
-    true_beta = 3
-    I = np.identity(np.dot(W,H).shape[1])
-    true_K = np.linalg.inv(I + true_beta * state_L)
-    d = np.dot(W, np.dot(H, true_K))
-    '''
-    
-    b_list = [0.1,0.25,0.5,0.75,1,3,5,7,9]
-    error_results = avg_train_test(np.array(state_norm.array), laplacian = state_L, test_list = [0.1, 0.2], runs = 10, 
-                        rank_list=range(1,15), beta_list=b_list)
-
-    #error_results = avg_train_test(d, laplacian = state_L, test_list = [0.05, 0.1, 0.2], runs = 10, algorithm="HoyerP")
-    
-    
-    reshaped_error = error_results.reshape(error_results.shape[0], -1)
-    np.savetxt("conv_parameter_results.csv", reshaped_error, delimiter = ',')
-    f = open("conv_error_shape.txt", "w+")
-    f.write(str(error_results.shape))
-    f.close()
+hrs = (end - start) / 60**2
+print("made it! Time : " + str(hrs) + " hrs")
