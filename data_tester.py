661fd688e93f12ab5cfdf6f72f0055f89a07da30
Kevin Quinn <quinnk@scc1.bu.edu>
2022-01-26 16:57:06 -0500

    random geometric laplacian

--- data_tester.py
+++ data_tester.py
@@ -12,15 +12,15 @@ import time
 n = 250
 m = 50
 rank = 4
-beta = 5
+beta = 10
 random_state = 1729
 noise_base = None
 
 # generate data using true paramters 
 W,H = gen_decomposition(n,m,rank, state=random_state)
 W,H = gen_decomposition(n,m,rank, state=random_state)
-G, laplacian = gen_laplacian(size = m, H = H, p_edge = 0.075, state=random_state)
-G, laplacian = gen_laplacian(size = m, H = H, p_edge = 0.075, state = random_state)
+G, laplacian = gen_laplacian(size = m, radius = 0.30, state=random_state)
+G, laplacian = gen_laplacian(size = m, radius = 0.30, state = random_state)
 K = np.linalg.inv(np.identity(m) + beta * laplacian)
 D = np.dot(W, np.dot(H, K))
 noise_D = add_noise(D, 0.001)
@@ -30,7 +30,7 @@ ranks = list(range(1,20))
 betas = np.linspace(1,20,20)
 
 start = time.time()
-G = gridSearcher(noise_D, laplacian = laplacian, algorithm = "diffusion", max_iter = 100000, tolerance = 1e-9, noise = noise_base, validate = 10, saver = "./analysis/testing_data/grid_search_" + str(rank) + "_" + str(beta) + ".csv")
+G = gridSearcher(noise_D, laplacian = laplacian, algorithm = "diffusion", max_iter = 100000, tolerance = 1e-9, noise = noise_base, validate = 10, saver = "./analysis/testing_data/grid_search_" + str(rank) + "_" + str(beta) + "_geo.csv")
 G.grid_search(ranks, betas)
 end = time.time()
 
